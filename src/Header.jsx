de3dede94d7bc9857bc222ed791a97904830e30b
Dicksen Tan <tandicksen@gmail.com>
2024-02-26 22:55:24 +0700

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <header>
+            <p>myBlog</p>
+            <ul className="linkList">
+                <li className="link">Home</li>
+                <li className="link">Post</li>
+            </ul>
+        </header>
+    )
+}
+export default Header
